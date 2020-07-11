cf8315b84a2fb578a5b649ac11642ac4e0c99d32
ethan2212 <e0407555@u.nus.edu>
2020-07-11 17:40:13 +0800

    Updates

---
+++ precache-manifest.f63f8eaa2923bcef180ba3d67b2dd15e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7930b5ce659655b4708e7c7fe5cefe0",
+    "url": "/CodeSpark/index.html"
+  },
+  {
+    "revision": "5f7268f24ea7819c366b",
+    "url": "/CodeSpark/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8bfdc989627e58009323",
+    "url": "/CodeSpark/static/css/main.37d7e1ca.chunk.css"
+  },
+  {
+    "revision": "5f7268f24ea7819c366b",
+    "url": "/CodeSpark/static/js/2.72d289ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CodeSpark/static/js/2.72d289ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfdc989627e58009323",
+    "url": "/CodeSpark/static/js/main.5740daee.chunk.js"
+  },
+  {
+    "revision": "42bd1bceb9630c33715f",
+    "url": "/CodeSpark/static/js/runtime-main.0f76319b.js"
+  },
+  {
+    "revision": "2850ed08635bf2f4583955425bc56043",
+    "url": "/CodeSpark/static/media/About.2850ed08.gif"
+  },
+  {
+    "revision": "1c9cda2c6a3d6b7100a917268421e978",
+    "url": "/CodeSpark/static/media/Curriculum.1c9cda2c.gif"
+  },
+  {
+    "revision": "2c46c98a678ebd8bfdae1cbc56a78ae0",
+    "url": "/CodeSpark/static/media/Games.2c46c98a.png"
+  },
+  {
+    "revision": "4718b66821f405006049902f051cc37b",
+    "url": "/CodeSpark/static/media/Lessons.4718b668.png"
+  },
+  {
+    "revision": "106ee39006bb1dff758cdd84e29bfb04",
+    "url": "/CodeSpark/static/media/Logo.106ee390.jpeg"
+  },
+  {
+    "revision": "855ad002e3f02c2b67737345f11878a3",
+    "url": "/CodeSpark/static/media/Logo_light.855ad002.jpg"
+  },
+  {
+    "revision": "c910ff4f7953a0a6f6a542aea0412e52",
+    "url": "/CodeSpark/static/media/Overview.c910ff4f.gif"
+  },
+  {
+    "revision": "0271d40796bb265225a51a1e0e3103cb",
+    "url": "/CodeSpark/static/media/Projects.0271d407.png"
+  },
+  {
+    "revision": "3bcddf72df8dee9a9ceefdf859273419",
+    "url": "/CodeSpark/static/media/Projects.3bcddf72.gif"
+  },
+  {
+    "revision": "922048f312a904eefd41839578d505c5",
+    "url": "/CodeSpark/static/media/Test_img.922048f3.JPG"
+  },
+  {
+    "revision": "2bb36a8a60b302fe97c1551a0ffe021f",
+    "url": "/CodeSpark/static/media/Welcome.2bb36a8a.png"
+  }
+]);
