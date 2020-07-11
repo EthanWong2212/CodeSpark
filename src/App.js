fd5821fea19f33cff74cc9fd737255cb3d97b345
ethan2212 <e0407555@u.nus.edu>
2020-07-11 18:28:38 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, HashRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from './Pages/components/Logo/Logo';
 import Navbar from './Pages/components/Navbar/Navbar';
@@ -10,14 +10,14 @@ function App(){
     
     return(
         <div>
-            <Router>
+            <HashRouter basename='/'>
                 <div>
                     <Logo/>
                 </div>
                     
                     <Navbar/>
                     <PageRouter/>
-            </Router>
+            </HashRouter>
         </div>
     )
 }
