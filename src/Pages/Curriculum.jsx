1d7f0c8120e2b73e287ae338190a769333b04d3c
ethan2212 <e0407555@u.nus.edu>
2020-07-13 21:49:00 +0800

    first commit

--- src/Pages/Curriculum.jsx
+++ src/Pages/Curriculum.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from './components/Page_header_gif/Curriculum.gif';
-import Gameindex from './components/Games/Functions/Gameindex';
+import Game from './components/Game';
 function Curriculum(){
     return(
         <div>
             <img className="Headergif" src={Header}/>
-            <span className="GameContainer"> 
-                <Gameindex/>
-            </span>
+            <Game/>
         </div>
         
-    )
+    );
     
 }
 
