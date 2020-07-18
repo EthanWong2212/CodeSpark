2f7a48afcfe474245dd3edd294e6dc06aab3bcdf
ethan2212 <e0407555@u.nus.edu>
2020-07-19 04:36:05 +0800

    Updates

---
+++ precache-manifest.4ff9c17c411ea530995f70a061876216.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87373111db471f930dcd62960995c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1818375d33aaef44e3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "52b13396aa03c21f891e",
+    "url": "/static/css/main.b8f7c0c5.chunk.css"
+  },
+  {
+    "revision": "2e1818375d33aaef44e3",
+    "url": "/static/js/2.fb0f91a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fb0f91a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b13396aa03c21f891e",
+    "url": "/static/js/main.bb8cfd60.chunk.js"
+  },
+  {
+    "revision": "dc2f04be104c9c6df3e0",
+    "url": "/static/js/runtime-main.bfd75ac0.js"
+  },
+  {
+    "revision": "2850ed08635bf2f4583955425bc56043",
+    "url": "/static/media/About.2850ed08.gif"
+  },
+  {
+    "revision": "db9648b4ee6d1945927dc7f8b82733c6",
+    "url": "/static/media/Curriculum.db9648b4.png"
+  },
+  {
+    "revision": "922048f312a904eefd41839578d505c5",
+    "url": "/static/media/Games.922048f3.JPG"
+  },
+  {
+    "revision": "4c232f7580eb24fb5515bf8acc613070",
+    "url": "/static/media/Home.4c232f75.png"
+  },
+  {
+    "revision": "6df1d1bd6e5862d9633ef3885ce7c0cb",
+    "url": "/static/media/Lessons.6df1d1bd.png"
+  },
+  {
+    "revision": "106ee39006bb1dff758cdd84e29bfb04",
+    "url": "/static/media/Logo.106ee390.jpeg"
+  },
+  {
+    "revision": "855ad002e3f02c2b67737345f11878a3",
+    "url": "/static/media/Logo_light.855ad002.jpg"
+  },
+  {
+    "revision": "c910ff4f7953a0a6f6a542aea0412e52",
+    "url": "/static/media/Overview.c910ff4f.gif"
+  },
+  {
+    "revision": "3bcddf72df8dee9a9ceefdf859273419",
+    "url": "/static/media/Projects.3bcddf72.gif"
+  },
+  {
+    "revision": "2bb36a8a60b302fe97c1551a0ffe021f",
+    "url": "/static/media/Welcome.2bb36a8a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CodeSpark/precache-manifest.cd6bb251cd14d53634f172e65475f8b4.js"
+  "/precache-manifest.4ff9c17c411ea530995f70a061876216.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CodeSpark/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
